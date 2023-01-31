d110f2894d61533ae2102fa9e4b20a9d1e2f806d
Siarhei Salimau <ssalimau@exadel.com>
2023-01-31 21:54:58 +0300

    FRONT-230131 added auth, no-auth guards

---
+++ heriatage-fe/src/app/routes/no-auth.guard.ts
@@ -0,0 +1,23 @@
+import { inject, Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable, tap } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { isUserLogged } from '../state/user.reducer';
+import { map } from 'rxjs/operators';
+
+@Injectable()
+export class NoAuthGuard implements CanActivate {
+  store = inject(Store);
+  router = inject(Router);
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> {
+    return this.store.select(isUserLogged).pipe(
+      map((isLogged: boolean) => !isLogged),
+      tap((isNotLogged: boolean) => {
+        if (!isNotLogged) {
+          this.router.navigateByUrl('/tree');
+        }
+      }),
+    );
+  }
+}
--- heriatage-fe/src/app/routes/routes.ts
+++ heriatage-fe/src/app/routes/routes.ts
@@ -1,30 +1,37 @@
 import { Routes } from '@angular/router';
 import { NotFoundPageComponent } from '../pages/not-found-page/not-found-page.component';
+import { AuthGuard } from './auth.guard';
+import { NoAuthGuard } from './no-auth.guard';
 
 export const routes: Routes = [
   {
     path: '',
     title: 'Homepage',
     loadComponent: () => import('./index').then(c => c.default),
+    providers: [AuthGuard, NoAuthGuard],
     children: [
       {
         path: 'authenticate',
         title: 'Authenticate user',
+        canActivate: [NoAuthGuard],
         loadComponent: () => import('../pages/authenticate/authenticate.component').then(c => c.AuthenticateComponent),
       },
       {
         path: 'login',
         title: 'Login user',
+        canActivate: [NoAuthGuard],
         loadComponent: () => import('../pages/login/login.component').then(c => c.LoginComponent),
       },
       {
         path: 'information',
         title: 'General user information',
+        canActivate: [AuthGuard],
         loadComponent: () => import('../pages/information/information.component').then(c => c.InformationComponent),
       },
       {
         path: 'tree',
         title: 'Family tree for user',
+        canActivate: [AuthGuard],
         loadComponent: () => import('../pages/tree/tree.component').then(c => c.TreeComponent),
       },
     ],
